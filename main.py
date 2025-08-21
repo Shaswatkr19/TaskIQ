59254a28ca97dfc2f05f090ca9ac8b428a16ce46
shaswatkr19 <shaswatsinha000@gmail.com>
2025-08-22 00:22:54 +0530

    Update some changes

--- main.py
+++ main.py
@@ -6,19 +6,33 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_openai_tools_agent, AgentExecutor 
+from todoist_api_python.api import TodoistAPI
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-@tool
-def add_task(task):
-    """Add a new task to the user's task list. Use this when the user wants to add or create a task"""
-    print(task)
-    print("Task added")
+todoist = TodoistAPI(todoist_api_key)
 
-tools = [add_task]
+@tool
+def add_task(task, desc):
+    """Add a new task to the user's task list. Use this when the user wants to add or create a task"""
+    todoist.add_task(content=task,
+                     description=desc)
+    
+
+@tool
+def show_tasks():
+    """show all tasks from Todoist. Use this tool when the user wants to see their tasks."""
+    results_paginator = todoist.get_tasks()
+    tasks = []
+    for task_list in results_paginator:
+        for task in task_list:
+           tasks.append(task.content)
+    return tasks       
+
+tools = [add_task, show_tasks]
 
 llm = ChatGoogleGenerativeAI(
     model='gemini-2.5-flash',
@@ -26,13 +40,18 @@ llm = ChatGoogleGenerativeAI(
     temperature=0.3
 )
 
-system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "Add a new task to buy milk"
+system_prompt = """You are a helpful assistant. 
+You will help the user add tasks.
+You will help the user show existing tasks. If the user asks to show the tasks: for example, "show me the tasks"
+print out the tasks to the user. Print them in a bullet list format.
+"""
+
 
 prompt = ChatPromptTemplate.from_messages([
     ("system", system_prompt),
-    ("user", user_input),
-    MessagesPlaceholder(variable_name="agent_scratchpad"),
+    MessagesPlaceholder("history"),
+    ("user", "{input}"),
+    MessagesPlaceholder(variable_name="agent_scratchpad") 
 ])
 
 # chain = prompt | llm | StrOutputParser()
@@ -40,6 +59,10 @@ agent = create_openai_tools_agent(llm, tools, prompt)
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=False)
 
 # response = chain.invoke({"input": user_input})
-response = agent_executor.invoke({"input": user_input})
-
-print(response['output'])
+history = []
+while True:
+    user_input = input("You: ")
+    response = agent_executor.invoke({"input": user_input, "history": history})
+    print(response['output'])
+    history.append(HumanMessage(content=user_input))
+    history.append(AIMessage(content=response['output']))
